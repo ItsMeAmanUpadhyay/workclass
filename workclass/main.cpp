3e1d56c4499da878a83c38f01846bd9411256d2a
ItsMeAmanUpadhyay <71437805+ItsMeAmanUpadhyay@users.noreply.github.com>
2021-01-29 10:45:03 +0530

    Initial Commit

---
+++ workclass/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  workclass
+//
+//  Created by Aman Upadhyay on 29/01/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
